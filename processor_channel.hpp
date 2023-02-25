09f5a341518543fd4e23b2e067e91236e8bf643b
chanhpar <chanhpar@student.42seoul.kr>
2023-02-25 22:53:33 +0900

    WIP: processor_*.hpp. processor_channel done, 나머지 해야함

---
+++ processor_channel.hpp
@@ -0,0 +1,101 @@
+/* Any copyright is dedicated to the Public Domain.
+ * https://creativecommons.org/publicdomain/zero/1.0/ */
+
+#pragma once
+
+#include "processor_base.hpp"
+
+namespace ft
+{
+    namespace irc
+    {
+        class processor_join : public processor_base
+        {
+        public:
+            int get_min_params() const throw() { return 1; }
+
+            void execute(const ft::irc::user& user, const ft::irc::message& message)
+            {
+                // FIXME
+            }
+        };
+
+        class processor_part : public processor_base
+        {
+        public:
+            int get_min_params() const throw() { return 1; }
+
+            void execute(const ft::irc::user& user, const ft::irc::message& message)
+            {
+                // FIXME
+            }
+        };
+
+        class processor_mode : public processor_base
+        {
+        public:
+            int get_min_params() const throw() { return 1; } // FIXME two commands: channel mode vs user mode
+
+            void execute(const ft::irc::user& user, const ft::irc::message& message)
+            {
+                // FIXME
+            }
+        };
+
+        class processor_topic : public processor_base
+        {
+        public:
+            int get_min_params() const throw() { return 1; }
+            int get_max_params() const throw() { return 2; }
+
+            void execute(const ft::irc::user& user, const ft::irc::message& message)
+            {
+                // FIXME
+            }
+        };
+
+        class processor_names : public processor_base
+        {
+        public:
+            void execute(const ft::irc::user& user, const ft::irc::message& message)
+            {
+                // FIXME
+            }
+        };
+
+        class processor_list : public processor_base
+        {
+        public:
+            int get_min_params() const throw() { return 1; }
+
+            void execute(const ft::irc::user& user, const ft::irc::message& message)
+            {
+                // FIXME
+            }
+        };
+
+        class processor_invite : public processor_base
+        {
+        public:
+            int get_min_params() const throw() { return 2; }
+            int get_max_params() const throw() { return 2; }
+
+            void execute(const ft::irc::user& user, const ft::irc::message& message)
+            {
+                // FIXME
+            }
+        };
+
+        class processor_kick : public processor_base
+        {
+        public:
+            int get_min_params() const throw() { return 2; }
+            int get_max_params() const throw() { return 3; }
+
+            void execute(const ft::irc::user& user, const ft::irc::message& message)
+            {
+                // FIXME
+            }
+        };
+    }
+}
---
+++ processor_register.hpp
@@ -0,0 +1,26 @@
+/* Any copyright is dedicated to the Public Domain.
+ * https://creativecommons.org/publicdomain/zero/1.0/ */
+
+#pragma once
+
+#include "irc_constants.hpp"
+
+#include "message.hpp"
+#include "user.hpp"
+
+namespace ft
+{
+    namespace irc
+    {
+        class processor_base
+        {
+        public:
+            virtual ~processor_base();
+            virtual int get_min_params();
+            virtual int get_max_params();
+            virtual bool is_registered_only();
+            virtual bool is_operator_only();
+            virtual void execute(const ft::irc::user& user, const ft::irc::message& message) = 0;
+        };
+    }
+}
