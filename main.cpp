a4d14ca86b82f2eb68ebfc469a6839c80fa839e3
jkong <jkong@student.42seoul.kr>
2023-03-02 12:58:36 +0900

    시그널 테스트

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@
 #include "server.hpp"
 #include "server_handler.hpp"
 
-#include <cstdlib>
 #include <libserv/libserv.hpp>
 #include <smart_ptr/smart_ptr.hpp>
+
+#include <csignal>
+#include <cstdlib>
 
 #if not FT_IRC_HARDWARE_CONCURRENCY > 0
 #include <unistd.h> // sysconf
@@ -27,6 +29,17 @@ namespace ft
     }
 }
 
+ft::shared_ptr<ft::serv::event_worker_group> boss_group;
+ft::shared_ptr<ft::serv::event_worker_group> child_group;
+
+static void _on_signal(int signo)
+{
+    static_cast<void>(signo);
+    std::cout << "signaled: " << signo << std::endl;
+    boss_group->shutdown_all();
+    child_group->shutdown_all();
+}
+
 int main(int argc, char* argv[])
 {
     // FIXME: 임시 코드
@@ -35,9 +48,8 @@ int main(int argc, char* argv[])
         std::cout << "Usage: " << argv[0] << " <port> <pass>" << std::endl;
         return EXIT_FAILURE;
     }
-    ft::shared_ptr<ft::serv::event_worker_group> boss_group = ft::make_shared<ft::serv::event_worker_group>();
-    ft::shared_ptr<ft::serv::event_worker_group> child_group = ft::make_shared<ft::serv::event_worker_group>();
-    // TODO: sigaction
+    boss_group = ft::make_shared<ft::serv::event_worker_group>();
+    child_group = ft::make_shared<ft::serv::event_worker_group>();
     boss_group->put_worker(ft::make_shared<ft::serv::event_worker>());
     const long hardware_concurrency =
 #if FT_IRC_HARDWARE_CONCURRENCY > 0
@@ -51,23 +63,28 @@ int main(int argc, char* argv[])
         child_group->put_worker(ft::make_shared<ft::serv::event_worker>());
     }
     ft::irc::server server(argv[2]);
+    ft::serv::bootstrap boot(boss_group, child_group, &ft::irc::_make_server, null);
+    std::signal(SIGINT, &_on_signal); // TODO: sigaction
+    try
     {
-        ft::serv::bootstrap boot(boss_group, child_group, &ft::irc::_make_server, null);
-        try
+        bool success = true;
+
+        if (!boot.start_server("", argv[1], &server))
         {
-            if (!boot.start_server("", argv[1], &server))
-            {
-                std::cerr << "bind failed. Is port \"" << argv[1] << "\" wrong?" << std::endl;
-                boss_group->shutdown_all();
-                child_group->shutdown_all();
-            }
+            std::cerr << "bind failed. Is port \"" << argv[1] << "\" wrong?" << std::endl;
+            success = false;
         }
-        catch (const ft::serv::syscall_failed& e)
+
+        if (success)
         {
-            std::cerr << "bind failed. System error occured: " << e.what() << std::endl;
-            boss_group->shutdown_all();
-            child_group->shutdown_all();
+            std::cout << "Server successfully started." << std::endl;
+            boot.set_success();
         }
     }
+    catch (const ft::serv::syscall_failed& e)
+    {
+        std::cerr << "bind failed. System error occured: " << e.what() << std::endl;
+    }
+    boot.finalize();
     return EXIT_SUCCESS;
 }
